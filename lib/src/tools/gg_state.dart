f3e74d0f99939a30b1d1c559eabe1a65c6b9c6a8
Gabriel Gatzsche <gabriel.gatzsche@grace.cloud>
2024-04-01 21:26:09 +0200

    GgState is now able to save the success state of check stages

---
+++ lib/src/tools/gg_state.dart
@@ -0,0 +1,114 @@
+// @license
+// Copyright (c) 2019 - 2024 Dr. Gabriel Gatzsche. All Rights Reserved.
+//
+// Use of this source code is governed by terms that can be
+// found in the LICENSE file in the root of this package.
+
+import 'dart:io';
+
+import 'package:gg_git/gg_git.dart';
+import 'package:gg_json/gg_json.dart';
+import 'package:gg_log/gg_log.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:path/path.dart';
+
+/// Stores and retrieves the state of the check commands
+class GgState {
+  /// Constructor
+  GgState({
+    required this.ggLog,
+    LastChangesHash? lastChangesHash,
+    IsCommitted? isCommitted,
+  }) : _lastChangesHash = lastChangesHash ?? LastChangesHash(ggLog: ggLog);
+
+  // ...........................................................................
+  /// The logger used for logging
+  final GgLog ggLog;
+
+  // ...........................................................................
+  /// Returns previously set value
+  Future<bool> readSuccess({
+    required Directory directory,
+    required String stage,
+    required GgLog ggLog,
+  }) async {
+    // Get the last changes hash
+    final changesHash = await _lastChangesHash.get(
+      directory: directory,
+      ggLog: ggLog,
+      ignoreFiles: ['.gg.json'],
+    );
+
+    // If no config file exists, return false
+    final fileExists =
+        await File(_configFile(directory: directory).path).exists();
+
+    if (!fileExists) {
+      return false;
+    }
+
+    // Get the hash written to .gg.json
+    final hashInCheckJson = await _ggJson.readFile<int>(
+      file: _configFile(directory: directory),
+      path: _hashPath(stage).join('/'),
+    );
+
+    // Compare the two hashes
+    // If they are the same, return true.
+    // If they are different, return false.
+    return changesHash == hashInCheckJson;
+  }
+
+  // ...........................................................................
+  /// Updates .gg.json and writes the success state for this stage.
+  Future<void> writeSuccess({
+    required Directory directory,
+    required String stage,
+  }) async {
+    // If success is already written, return
+    final isWritten = await readSuccess(
+      directory: directory,
+      stage: stage,
+      ggLog: ggLog,
+    );
+    if (isWritten) {
+      return;
+    }
+
+    // Get the hash of the current commit
+    final currentHash = await _lastChangesHash.get(
+      directory: directory,
+      ggLog: ggLog,
+      ignoreFiles: ['.gg.json'],
+    );
+
+    // Write the hash to .gg.json
+    await _ggJson.writeFile(
+      file: _configFile(directory: directory),
+      path: _hashPath(stage).join('/'),
+      value: currentHash,
+    );
+  }
+
+  // ######################
+  // Private
+  // ######################
+
+  final LastChangesHash _lastChangesHash;
+  final _ggJson = const GgJson();
+
+  // ...........................................................................
+  List<String> _hashPath(String name) => [name, 'success', 'hash'];
+
+  // ...........................................................................
+  File _configFile({
+    required Directory directory,
+  }) {
+    final filePath = join(directory.path, '.gg.json');
+    final file = File(filePath);
+    return file;
+  }
+}
+
+/// Mock for [GgState]
+class MockCheckState extends Mock implements GgState {}
---
+++ test/tools/gg_state_test.dart
@@ -0,0 +1,179 @@
+// @license
+// Copyright (c) 2019 - 2024 Dr. Gabriel Gatzsche. All Rights Reserved.
+//
+// Use of this source code is governed by terms that can be
+// found in the LICENSE file in the root of this package.
+
+import 'dart:convert';
+
+import 'dart:io';
+
+import 'package:gg/src/tools/gg_state.dart';
+import 'package:gg_git/gg_git.dart';
+import 'package:gg_git/gg_git_test_helpers.dart';
+import 'package:test/test.dart';
+
+void main() {
+  late Directory d;
+  late GgState ggState;
+  final messages = <String>[];
+
+  // ...........................................................................
+  void initCommand() {
+    ggState = GgState(
+      ggLog: messages.add,
+    );
+    initGit(d);
+  }
+
+  // ...........................................................................
+  setUp(() async {
+    messages.clear();
+    d = Directory.systemTemp.createTempSync();
+    await initGit(d);
+    initCommand();
+    await setPubspec(d, version: '1.0.0');
+    await commitPubspec(d);
+  });
+
+  // ...........................................................................
+  tearDown(() {
+    d.deleteSync(recursive: true);
+  });
+
+  // ...........................................................................
+  group('CheckState', () {
+    group('writeSuccess(directory, success)', () {
+      group('with success == true', () {
+        test('should write last change hash to .gg.json', () async {
+          await addAndCommitSampleFile(d);
+
+          // Get last changes hash
+          final hash = await LastChangesHash(ggLog: messages.add).get(
+            directory: d,
+            ggLog: messages.add,
+          );
+
+          // Set the state
+          await ggState.writeSuccess(
+            directory: d,
+            stage: 'can-commit',
+          );
+
+          // Check the file
+          final checkJson = File('${d.path}/.gg.json');
+          await expectLater(await checkJson.exists(), isTrue);
+          final contentsString = await checkJson.readAsString();
+          final contents = json.decode(contentsString);
+          expect(
+            contents['can-commit']['success']['hash'],
+            hash,
+          );
+        });
+      });
+    });
+
+    group('readSuccess(directory, stage, ggLog)', () {
+      group('should return', () {
+        group('false', () {
+          test('if no .gg.json exists', () async {
+            expect(await File('${d.path}/.gg.json').exists(), isFalse);
+            final result = await ggState.readSuccess(
+              directory: d,
+              ggLog: messages.add,
+              stage: 'can-commit',
+            );
+            expect(result, isFalse);
+          });
+
+          test('if .gg.json is empty', () async {
+            File('${d.path}/.gg.json').writeAsStringSync('{}');
+
+            final result = await ggState.readSuccess(
+              directory: d,
+              ggLog: messages.add,
+              stage: 'can-commit',
+            );
+            expect(result, isFalse);
+          });
+
+          test('if last success hash is not current hash', () async {
+            // Set the state
+            await ggState.writeSuccess(
+              directory: d,
+              stage: 'can-commit',
+            );
+
+            // Change the file
+            await addAndCommitSampleFile(d);
+
+            final result = await ggState.readSuccess(
+              directory: d,
+              ggLog: messages.add,
+              stage: 'can-commit',
+            );
+            expect(result, isFalse);
+          });
+        });
+
+        group('true', () {
+          test('if last success hash is current hash', () async {
+            // Commit something
+            await addAndCommitSampleFile(d, fileName: 'file0.txt');
+
+            // Write success after everything is committed
+            await ggState.writeSuccess(
+              directory: d,
+              stage: 'can-commit',
+            );
+
+            // Read succes -> It should be true
+            final result = await ggState.readSuccess(
+              directory: d,
+              ggLog: messages.add,
+              stage: 'can-commit',
+            );
+            expect(result, isTrue);
+
+            // Make a modification
+            await File('${d.path}/file0.txt').writeAsString('modified');
+
+            // Read success -> It should be false
+            final result2 = await ggState.readSuccess(
+              directory: d,
+              ggLog: messages.add,
+              stage: 'can-commit',
+            );
+            expect(result2, isFalse);
+
+            // Write success again
+            await ggState.writeSuccess(
+              directory: d,
+              stage: 'can-commit',
+            );
+
+            // Read success -> It should be true
+            final result3 = await ggState.readSuccess(
+              directory: d,
+              ggLog: messages.add,
+              stage: 'can-commit',
+            );
+            expect(result3, isTrue);
+
+            // Commit the last changes.
+            // This should not change the success state.
+            await commitFile(d, 'file0.txt');
+
+            // Read success -> It should be true
+            final result4 = await ggState.readSuccess(
+              directory: d,
+              ggLog: messages.add,
+              stage: 'can-commit',
+            );
+            expect(result4, isTrue);
+          });
+        });
+      });
+    });
+  });
+}
